11f556410477982535f9ac607b40f7d2cad93c3f
shinny2 <adamshin2003@gmail.com>
2025-04-11 14:01:16 -0400

    javascript

--- library.js
+++ library.js
@@ -1,24 +1,89 @@
 const myLibrary = [];
+const libraryContainer = document.querySelector("#libraryContainer");
+const openDialogBtn = document.querySelector("#openDialogBtn");
+const bookDialog = document.querySelector("#bookDialog");
+const bookForm = document.querySelector("#bookForm");
+const submitBookBtn = document.querySelector("#submitBookBtn");
 
-function Book(title, author, pages, read) {
-    if(!new.target) {
-        throw Error("Need new keyword.");
-    }
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-
-    this.info = function () {
-        if(read) {
-            return `${title} by ${author}, ${pages} pages, already read.`;
-        }else {
-            return `${title} by ${author}, ${pages} pages, not read yet.`;
-        }
+class Book{
+    constructor(title, author, pages, read) {
+        this.id=crypto.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
 }
 
 function addBookToLibrary (title, author, pages, read) {
-    let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
+    const newBook = new Book(title, author, pages, read);
+    library.push(newBook);
+    renderLibrary();
 }
+
+function removeBook(id) {
+    const index = library.findIndex(book => book.id==id);
+    if(index != -1) {
+        library.splice(index, 1);
+        renderLibrary();
+    }
+}
+
+function toggleReadStatus(id) {
+    const book = library.find(book => book.id=id);
+    if(book) {
+        book.read = book.read === "Read" ? "Not Read" : "Read";
+        renderLibrary();
+    }
+}
+
+function createBookCard(book) {
+    const card = document.createElement("div");
+    card.classList.add("bookCard");
+    card.dataset.id = book.id;
+
+    const title = document.createElement("p");
+    title.textContent = book.title;
+
+    const author = document.createElement("p");
+    author.textContent = `Written by:<br>${book.author}`;
+
+    const pages = document.createElement("p");
+    pages.textContent = `${book.pages} pages`;
+
+    const readBadge = document.createElement("p");
+    readBadge.classList.add("readStatus");
+    readBadge.textContent = book.read;
+
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("removeBtn");
+    removeBtn.textContent = "Remove";
+    removeBtn.addEventListener("click", () => removeBook(book.id));
+
+    const toggleBtn = document.createElement("button");
+    toggleBtn.textContent = "Toggle Read";
+    toggleBtn.addEventListener("click", ()=> toggleReadStatus(book.id));
+
+    card.append(title, author, pages, readBadge, removeBtn, toggleBtn);
+    libraryContainer.append(card);
+}
+
+function renderLibrary() {
+    libraryContainer.innerHTML="";
+    library.forEach(createBookCard);
+}
+
+openDialogBtn.addEventListener("click",()=>book.Dialog.showModal());
+
+submitBookbtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const {title, author, pages, readBadge, removeBtn, toggleBtn} = bookForm;
+    addBookToLibrary(
+        title.value,
+        author.value,
+        pages.value,
+        read.checked ? "Read" : "Not Read"
+    )
+    bookDialog.close();
+    bookForm.reset();
+})
